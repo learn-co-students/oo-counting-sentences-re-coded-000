f91cc1c642afad3aa7eaae4bda63ebdf5b0384d9
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-10-20 19:39:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
-
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr= []
+    splt_arr= self.split(/[!|.|?]/)
+    arr= splt_arr.reject do |word|  #reject returns an array with the elements for which the block is false
+      word.to_s.empty?
+    end
+    return arr.size
   end
 end
