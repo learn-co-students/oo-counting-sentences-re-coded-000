a03f703af2b3ecd44d939b4aa4b0e7d57dc5585a
Ayedh Gaser <alshmary_ad@yahoo.com>
2016-10-28 12:20:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence=self.split(/\.|\?|\!/)
+    sentence.each do |item|
+     if item.length < 2 then
+       sentence.delete(item)
+     end
+    end
+    return sentence.size
   end
 end
